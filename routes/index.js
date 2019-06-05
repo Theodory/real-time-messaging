f45567f03c93fa1f7d8bf332ce002ff6f73e68b0
Theodory <theodoryf@gmail.com>
2019-06-05 18:39:19 +0300

    install request

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,22 @@
 var express = require('express');
-var axios = require('axios');
+var request = require('request');
 var router = express.Router();
 
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	res.render('index', { weather: '' });
+	res.render('index', { weather: null, error: null });
 });
 
 router.post('/',  (req, res) => {
 	let form = req.body.city
 	let url = `http://api.openweathermap.org/data/2.5/weather?q=${form}&appid=${process.env.MAP_ORG_API_KEY}`
 
-	axios.post(url)
-	.then((body) => {
+	request(url,(err,response,body) => {
 		let data = JSON.parse(body)
-		let textMessage = `Here at ${data.name} the temperature is ${data.main.temp}`
+		let textMessage = `Here at ${data.name } weather is ${data.main.temp}`
 		res.render('index',{weather: textMessage});
-	})
-	.catch((error) => {
-		let textMessage = `Here at ${data.name} the temperature is ${data.main.temp}`
-		res.render('index',{error: 'Error occured!'});
 	})
 
 
