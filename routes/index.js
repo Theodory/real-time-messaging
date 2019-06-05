a1aabdecd97b8d953a0ea566157ac12fc27d5218
Theodory <theodoryf@gmail.com>
2019-06-05 15:49:50 +0300

    Add index html view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var request = require('request');
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/',  (req, res) => {
+	let form = req.body.city
+	let url = `http://api.openweathermap.org/data/2.5/weather?q=${form}&appid=${process.env.MAP_ORG_API_KEY}`
+
+	request(url,(err,response,body) => {
+		let data = JSON.parse(body)
+		let textMessage = `Here at ${data.name } weather is ${data.main.temp}`
+		res.render('index',{weather: textMessage});
+	})
+})
+
 
 module.exports = router;
