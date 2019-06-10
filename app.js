fb4576031c9e1ce5eaf7045b0811a8cd41d8affe
Theodory <theodoryf@gmail.com>
2019-06-10 10:35:51 +0300

    Add database enviromental variables

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-let dotenv = require('dotenv').config()
+const dotenv = require('dotenv').config();
+const mysql = require('mysql');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
