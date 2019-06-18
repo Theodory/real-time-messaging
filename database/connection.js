783a68cdf5a0d0e8a2a77cf28263a8e36a58ae4e
Theodory <theodoryf@gmail.com>
2019-06-18 08:52:36 +0300

    Configure sequalize

--- database/connection.js
+++ database/connection.js
@@ -1,15 +1,21 @@
 const express = require('express');
-const mysql = require('mysql');
+const Sequelize = require('sequelize');
 const dotenv = require('dotenv').config();
 
-const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  port: process.env.DB_PORT,
-});
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_DATABASE,
+//   port: process.env.DB_PORT,
+// });
 
 connection.connect();
+
+// Option 1: Passing parameters separately
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect:  process.env.DB_DIALECT, 
+});
 
 module.exports = connection;
