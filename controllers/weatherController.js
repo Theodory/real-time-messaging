53ebfe92a4973dc9e86c70be5d5cb2faba64a85c
Theodory <theodoryf@gmail.com>
2019-06-22 18:06:44 +0300

    Add promises

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -14,10 +14,18 @@ exports.getWeather  =  (req, res) => {
 	let form = req.body.city
 	let url = `http://api.openweathermap.org/data/2.5/weather?q=${form}&appid=${process.env.MAP_ORG_API_KEY}`
 
-	request(url,(err,response,body) => {
-		let data = JSON.parse(body)
-		let textMessage = `Here at ${data.name } weather is ${data.main.temp}`
-		res.render('index',{weather: textMessage});
+	request(url, async (err,response,body) => {
+
+		return new Promise((resolve,reject) => {
+
+				let data = JSON.parse(body)
+				let textMessage = `Here at ${data.name } weather is ${data.main.temp}`
+				res.render('index',{weather: textMessage,error: null});
+			
+
+		}).catch(err=> {
+			console.log(err)
+		})
 	})
 
 	
