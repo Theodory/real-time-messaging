7040f88291c338ebc9092bf25187ed428345c264
Theodory <theodoryf@gmail.com>
2019-06-22 19:03:05 +0300

    Handle http errord

--- controllers/weatherController.js
+++ controllers/weatherController.js
@@ -14,30 +14,30 @@ exports.getWeather  =  (req, res) => {
 	let form = req.body.city
 	let url = `http://api.openweathermap.org/data/2.5/weather?q=${form}&appid=${process.env.MAP_ORG_API_KEY}`
 
-	request(url, async (err,response,body) => {
 
-		return new Promise((resolve,reject) => {
-
-			if (err) {
-
-				reject()
-
-			} else {
-
-				resolve(() => {
+	return new Promise((resolve,reject) => {
+		try {
+			request(url, async (err,response,body) => {
+				if(err){
+					let textMessage = `Error occured`
+					res.render('index',{weather: null,error: textMessage});
+				}else{
 					let data = JSON.parse(body)
-				let textMessage = `Here at ${data.name } weather is ${data.main.temp}`
-				res.render('index',{weather: textMessage,error: null});
-				})
-			}
-			
+					if(data.main === undefined){
+						let textMessage = `Error occured`
+					res.render('index',{weather: null,error: textMessage});
+					}else{
+						let textMessage = `Here at ${data.name } weather is ${data.main.temp}`
+						res.render('index',{weather: textMessage,error: null});
+					}
+				}
 
-		}).catch((error) => {
-
-				let textMessage = `Error occured`
-				res.render('index',{error: textMessage,weather: null});
-
-		});
+			})
+		}
+		catch(error) {
+			let textMessage = `Error occured`
+			res.render('index',{weather: null,error: textMessage});
+		}
 	})
 
 	
